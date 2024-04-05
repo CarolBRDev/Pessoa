7aa1f6eaa1dbb32c07d217a5b10d8b98eccbee53
Caroline Ramos <carolbeatrice@gmail.com>
2024-04-04 21:33:34 -0300

    Projeto para cadastro de pessoas, com construtor e metodo Apresentar()

---
+++ Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pessoa
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public double Altura;
+
+        public Pessoa(string nome, int idade, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}\nIdade: {Idade} anos\nAltura: {Altura:F2} m");
+        }
+    }
+}
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite o nome da pessoa:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade da pessoa:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura da pessoa:");
+            double altura = double.Parse(Console.ReadLine());
+
+            Pessoa pessoa1 = new Pessoa(nome, idade, altura);
+            pessoa1.Apresentar();
+        }
+    }
+}
